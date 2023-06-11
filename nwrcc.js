496bf02f53a164d27d0d40963aafae2c98c02824
Tristan Sunico <tristan.sunico@gmail.com>
2023-06-10 22:44:10 -0400

    Update nwrcc.js

--- nwrcc.js
+++ nwrcc.js
@@ -23,7 +23,7 @@ AutoCookie.onLogic = function() {
             if(!AutoCookie.config.miscAch || ((!Game.HasAchiev("Fading luck") && shimmer.life<Game.fps) || Game.shimmers.length > 1) || Game.HasAchiev("Fading luck"))
                 if(shimmer.type == "golden" || AutoCookie.config.clickWrath) shimmer.pop();
         });
-}
+};
 
 AutoCookie.onCheck = function() {
     if(AutoCookie.config.miscAch) {
